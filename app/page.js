7547fc70deb94cd5f7e09c67335e223dc7149fd4
MgZayarNaing <zayarnaing.st@gmail.com>
2024-10-19 15:28:10 +0630

    update

--- app/page.js
+++ app/page.js
@@ -1,10 +1,48 @@
+"use client";
 import Link from 'next/link'
 import React from 'react'
+import { TypeAnimation } from "react-type-animation";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Pagination, Navigation, Autoplay } from 'swiper/modules';
+import Image from 'next/image';
+import logo from  '../public/logo.webp';
+import logo1 from  '../public/logo.webp';
 
 const page = () => {
   return (
     <div>
-      <Link  href="/about">About</Link>
+      <Swiper
+         spaceBetween={50}
+         slidesPerView={1}
+         pagination={{ clickable: true }}
+         loop
+         autoplay={{ delay: 3000, disableOnInteraction: false }}
+         modules={[Pagination, Navigation, Autoplay]}
+      >
+        <SwiperSlide><Image src={logo}></Image></SwiperSlide>
+        <SwiperSlide><Image src={logo1}></Image></SwiperSlide>
+        <SwiperSlide><Image src={logo}></Image></SwiperSlide>
+        <SwiperSlide><Image src={logo}></Image></SwiperSlide>
+
+      </Swiper>
+      <Link href="/about">About</Link>
+
+      <TypeAnimation
+        sequence={[
+          'Time In The Market, Beats Timing The Market.', // Types the string
+          1000, // Waits 1s
+          '', // Deletes everything
+          1000, // Waits 1s before typing again
+          'Time In The Market, Beats Timing The Market.', // Types the string again
+        ]}
+        wrapper="h1"
+        cursor={true}
+        repeat={Infinity}
+        className=' text-4xl font-mono font-semibold text-red-500 text-center'
+      />
+
+
     </div>
   )
 }
